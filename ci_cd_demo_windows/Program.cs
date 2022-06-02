b140219549ac53bc65ab80cde27df08ae65c4576
amarnathr210 <amarnathr210@gmail.com>
2022-06-02 11:30:20 +0530

    pushing the changes

---
+++ ci_cd_demo_windows/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ci_cd_demo_windows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
